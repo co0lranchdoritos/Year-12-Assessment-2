02c5ff7f0628101af362ad7876522c01a9ee14b9
RJ <riley.bell6@education.nsw.gov.au>
2026-07-28 01:00:20 +1000

    timer i give up nithinsg working

--- JS/timer.js
+++ JS/timer.js
@@ -3,24 +3,23 @@ const stop = doccument.getElementById("stop");
 const reset = doccument.getElementById("reset");
 const timer = doccument.getElementById("timer");
 
-let timeleft = 1500;
+let timeLeft = 1500;
 let interval;
 
 const updateTimer = () => {
-    const minutes = Math.floor(timeleft / 60);
-    const seconds = timeleft % 60;
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
 
-    timer.innerHTML = '$(minutes.toString().padStart(2,"0")):$(seconds)';
+    timer.innerHTML = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
  };
 
  const startTimer = () => {
     interval = setInterval(() => {
-        timerLeft--;
+        timeLeft--;
         updateTimer();
 
         if (timeLeft === 0){
             clearInterval(interval);
-            alert("Times up!")
             timeLeft = 1500;
             updateTimer();
         }
