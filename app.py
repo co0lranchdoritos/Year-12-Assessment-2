2f4a2a96d282ec14f1377c5293602fdffe1431a3
RJ <riley.bell6@education.nsw.gov.au>
2026-07-29 10:51:38 +1000

    troubleshooting

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 app.secret_key = "supersecretkey"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Year-12-Assessment-2.db"  
